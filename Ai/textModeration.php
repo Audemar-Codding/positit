fa22de886ee5d15c8c74b4228519acb39bccc073
Audemar-Coding <83353033+Audemar-Codding@users.noreply.github.com>
2024-08-28 16:57:45 +0200

    Add files via upload

---
+++ Ai/textModeration.php
@@ -0,0 +1,43 @@
+<?php
+namespace Ai;
+
+require_once('./vendor/autoload.php');
+
+class textModeration {
+  
+  private $client;
+
+  public function __construct()
+  {
+    $this->client = new \GuzzleHttp\Client();
+  }
+  
+  
+    public function isSafe($text, $lang) {
+$response = $this->client->request('POST', 'https://api.edenai.run/v2/text/moderation', [
+  'body' => '{"response_as_dict":true,"attributes_as_list":false,"show_base_64":true,"show_original_response":false,"providers":"openai","language":"'.$lang.'","text":"'.$text.' test"}',
+  'headers' => [
+    'accept' => 'application/json',
+    'authorization' => 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZDBhOTk2YzUtMTNhMy00M2EzLTliNDMtMDRkYzc4Y2JkOTZlIiwidHlwZSI6ImFwaV90b2tlbiJ9.vlviSKNAjMxqphstROhWUOdg-p2h3HlXVhOBu2i_PN0',
+    'content-type' => 'application/json',
+  ],
+]);
+
+$Arrayresponse = $response->getBody();
+
+$data = json_decode($Arrayresponse, true);
+
+$label = [];
+
+    foreach ($data['openai']['items'] as $item) {
+        if ($item['likelihood'] >= 4) {
+        $label[] = $item['label'];
+        }
+    }
+
+return $label;
+}
+
+
+    
+}
